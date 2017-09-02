a4c71f807f439d52a63e5be04d6debae2790b7c1
jonieva <jorgeonieva@gmail.com>
2017-09-02 15:15:49 -0400

    ENH: added test for a whole study

--- data_tests.py
+++ data_tests.py
@@ -1,16 +1,23 @@
 import unittest
-from dataset import *
+from study_parser import *
 
 class DataTests(unittest.TestCase):
     def setUp(self):
+        """
+        Initialize all the settings needed to run the tests
+        """
         # Init folders
         self.dicom_files_folder = "final_data/dicoms/SCD0000101"
         self.contour_files_folder = "final_data/contourfiles/SC-HF-I-1/i-contours/"
-        self.previously_saved_mask_file = "final_data/IM-0001-0048-icontour-manual-mask.npy"
         self.contour_file_mask_format="IM-0001-{:04}-icontour-manual.txt"
         self.contour_file_name = os.path.join(self.contour_files_folder, self.contour_file_mask_format.format(48))
+        self.previously_saved_mask_file = os.path.join(self.contour_files_folder, "IM-0001-0048-icontour-manual-mask.npy")
+        self.total_images_in_study = 240
 
     def testReadSingleDicomFile(self):
+        """
+        Check the first dicom image in the study
+        """
         file_name = os.path.join(self.dicom_files_folder, "1.dcm")
         self.assertTrue(os.path.exists(file_name), "File '{}' not found. Please review test data folders".format(file_name))
         pixels = parse_dicom_file(file_name)
@@ -23,7 +30,9 @@ class DataTests(unittest.TestCase):
         self.assertTrue(pixels[100, 100] == 32, "Data in '{}' is not as expected. Corrupt image?".format(file_name))
 
     def testParseContourFile(self):
-        # Use for the test the first contours file
+        """
+        Read a contour file and get a list of coords
+        """
         self.assertTrue(os.path.exists(self.contour_file_name), "File '{}' not found. Please review test data folders".
                         format(self.contour_file_name))
         coords = parse_contour_file(self.contour_file_name)
@@ -34,7 +43,10 @@ class DataTests(unittest.TestCase):
         self.assertTrue(coords[0][0] == 120.50 and coords[0][1] == 137.50 \
                         and coords[149][0] == 120.50 and coords[149][1] == 138.00)
 
-    def testParseMaskSyntetic(self):
+    def testParseMaskSynthetic(self):
+        """
+        Test the method to get a boolean mask from a list of coords, using a small synthetic dataset
+        """
         # Build a squared mask
         coords = [(0, 0), (0, 7), (7, 7), (7,0)]
         mask = poly_to_mask(coords, 10, 10)
@@ -44,8 +56,11 @@ class DataTests(unittest.TestCase):
         self.assertTrue(mask.sum() == 36)
 
     def testCompareWithKnownMask(self):
-        file_name = os.path.join(self.contour_files_folder, self.contour_file_mask_format.format(48))
-        coords = parse_contour_file(file_name)
+        """
+        Test the method to get a boolean mask from a list of coords, using as a reference a mask numpy array that
+        was previously validated
+        """
+        coords = parse_contour_file(self.contour_file_name)
         # Get a mask from the contour
         mask1 = poly_to_mask(coords, 256, 256)
         # Load a previously saved mask
@@ -54,7 +69,18 @@ class DataTests(unittest.TestCase):
         self.assertTrue(np.array_equal(mask1, mask2), "Previously calculated mask does not match with the current results")
 
     def testLoadFullStudy(self):
-        self.fail("Not implemented!")
+        i = 0
+        for im, mask in process_study(self.dicom_files_folder, self.contour_files_folder,
+                                        contour_file_mask_format=self.contour_file_mask_format):
+            self.assertIsNotNone(im)
+            self.assertIsNotNone(mask)
+            self.assertIsInstance(im, np.ndarray, "Error in image {}".format(i))
+            self.assertIsInstance(mask, np.ndarray, "Error in image {}".format(i))
+            self.assertTrue(im.dtype == np.int16, "Error in image {}".format(i))
+            self.assertTrue(mask.dtype == np.bool, "Error in image {}".format(i))
+            i += 1
+        self.assertTrue(i == self.total_images_in_study, "Expected a total of {} images, but got {}".
+                        format(self.total_images_in_study, i))
 
 def main():
     unittest.main()
